fc728918b61f8b167df46fc4abadc0ce7469a9df
Lipotam <Lipotam@gmail.com>
2015-01-20 22:13:44 +0300

    code is separated into several files

--- code/PABS_sketch/PABS_sketch.ino
+++ code/PABS_sketch/PABS_sketch.ino
@@ -1,27 +1,6 @@
-int adminStartButton = 0;
-int adminResetButton = 1;
-int adminInterrupt = 2;
-int playerInerrupt = 3;
-int firstPlayerButton = 4;
-int secondPlayerButton = 5;
-int thirdPlayerButton = 6;
-int fourthPlayerButton = 7;
-int fifthPlayerButton = 8;
-int speakerButton = 9;
-int ledShiftRegisterClk = 10;
-int ledShiftRegisterData = 11;
-int ledShiftRegisterRefresh = 12;
-
-int led = 13;
-
-int displayShiftRegisterClk = A0;
-int displayShiftRegisterData = A1;
-int displayShiftRegisterRefresh = A2;
-int gameStateHighBit = A3;
-int gameStateLowBit = A4;
+#include "constants.h"
 
 
-byte digit[11] = {0b00001000,0b00111110,0b00010001,0b00010100, 0b00100110, 0b01000100, 0b01000000, 0b00011110,0b00000000,0b00000100, 0b01111111};
 
 void setup() {                
   pinMode(led, OUTPUT);     
